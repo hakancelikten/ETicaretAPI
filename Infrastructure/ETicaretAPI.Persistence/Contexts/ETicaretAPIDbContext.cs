4e1e9ab260b9ef93d28a2f922ab3315bba634a8e
cansu çelikten <cansucelikten7@gmail.com>
2023-05-20 14:44:17 +0300

    SaveChangeAsync Interceptor

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace ETicaretAPI.Persistence.Contexts
@@ -6,10 +7,27 @@ namespace ETicaretAPI.Persistence.Contexts
     public class ETicaretAPIDbContext : DbContext
     {
         public ETicaretAPIDbContext(DbContextOptions options) : base(options)
-        {}
+        { }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //ChangeTracker: Entity'ler üzerinden yapılan değişiklilerin ya da yeni eklenen verinin yakalanmasını sağlayan yapıdır. update operasyonunda Track edilen verilerin yakalanıp elde etmenizi sağlar. 
+            // Biz herhangi bir veri eklenirken veya güncellenirken  savechangesasync tetiklersek burada ilk önce buradaki override tetiklenecek. gelen datalar yakalanacak, bu datalar üzerinde state'e göre düzenleyip ona göre savechanges çalıştırılacaktır.
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow,
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- Presentation/ETicaretAPI.Presentation/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.Presentation/Controllers/ProductsController.cs
@@ -24,22 +24,22 @@ namespace ETicaretAPI.Presentation.Controllers
         [HttpGet]
         public async Task Get()
         {
-            //await _productWriteRepository.AddRangeAsync(new()
-            //{
-            //    new (){Id=Guid.NewGuid(), Name="Product 1", Price=100, CreatedDate=DateTime.UtcNow,Stock=10},
-            //    new (){Id=Guid.NewGuid(), Name="Product 2", Price=200, CreatedDate=DateTime.UtcNow,Stock=20},
-            //    new (){Id=Guid.NewGuid(), Name="Product 3", Price=300, CreatedDate=DateTime.UtcNow,Stock=130}
-            //});
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new (){Id=Guid.NewGuid(), Name="Product 1", Price=100, Stock=10},
+                new (){Id=Guid.NewGuid(), Name="Product 2", Price=200, Stock=20},
+                new (){Id=Guid.NewGuid(), Name="Product 3", Price=300, Stock=130}
+            });
 
-            //var count = await _productWriteRepository.SaveAsync();
+            var count = await _productWriteRepository.SaveAsync();
 
             /*
              * burada tracking false gönderdiğimiz için mehmet değişikliği olmayacaktır.
              * aynı scope içerisinde savechange yaptığımızda bu değişiklik yansımayacaktır.
             */
-            Product p = await _productReadRepository.GetByIdAsync("2ca111cf-cac1-4ec0-9cf4-186638d6d1d7", false);
-            p.Name = "Mehmet";
-            await _productWriteRepository.SaveAsync();
+            //Product p = await _productReadRepository.GetByIdAsync("2ca111cf-cac1-4ec0-9cf4-186638d6d1d7", false);
+            //p.Name = "Mehmet";
+            //await _productWriteRepository.SaveAsync();
         }
     }
 }
