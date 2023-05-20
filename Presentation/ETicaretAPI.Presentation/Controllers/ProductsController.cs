ee55dc4b2db8685c7ba5c9b8e227f4ba430acf1f
cansu çelikten <cansucelikten7@gmail.com>
2023-05-20 14:11:16 +0300

    ..

--- Presentation/ETicaretAPI.Presentation/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.Presentation/Controllers/ProductsController.cs
@@ -1,6 +1,9 @@
 ﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.AccessControl;
 
 namespace ETicaretAPI.Presentation.Controllers
 {
@@ -8,18 +11,35 @@ namespace ETicaretAPI.Presentation.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductService _productService;
 
-        public ProductsController(IProductService productService)
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
         {
-            _productService = productService;
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
         }
 
         [HttpGet]
-        public IActionResult GetProducts()
+        public async Task Get()
         {
-            var products = _productService.GetProducts();
-            return Ok(products);
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new (){Id=Guid.NewGuid(), Name="Product 1", Price=100, CreatedDate=DateTime.UtcNow,Stock=10},
+            //    new (){Id=Guid.NewGuid(), Name="Product 2", Price=200, CreatedDate=DateTime.UtcNow,Stock=20},
+            //    new (){Id=Guid.NewGuid(), Name="Product 3", Price=300, CreatedDate=DateTime.UtcNow,Stock=130}
+            //});
+
+            //var count = await _productWriteRepository.SaveAsync();
+
+            /*
+             * burada tracking false gönderdiğimiz için mehmet değişikliği olmayacaktır.
+             * aynı scope içerisinde savechange yaptığımızda bu değişiklik yansımayacaktır.
+            */
+            Product p = await _productReadRepository.GetByIdAsync("2ca111cf-cac1-4ec0-9cf4-186638d6d1d7", false);
+            p.Name = "Mehmet";
+            await _productWriteRepository.SaveAsync();
         }
     }
 }
